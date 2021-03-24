51dea0e93b06287e7c14f4a8b451b756f38eb35b
Vignesh <vignesh.krish1989@gmail.com>
2021-03-24 11:12:35 +0530

    Initial Commit

---
+++ LoginValidationTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginValidationTest
+//
+//  Created by Vignesh Krishnamurthy on 24/03/21.
+//  Copyright © 2021 vignesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
